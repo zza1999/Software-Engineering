ff5c4d7e67aab356ab23f85078e7f0d170e2d292
skrkuromi <987186163@qq.com>
2020-06-06 09:17:52 +0800

    配置antd、echarts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'antd';
 import './App.css';
 
 class App extends React.Component {
@@ -6,22 +7,32 @@ class App extends React.Component {
 		articleList: null
 	}
 
-	componentWillMount() {
-		fetch('/articles/')
-			.then(response => response.json())
-			.then(result => {
-				this.setState({ articleList: result })
-				console.log(result)
-			})
-			.catch(e => e);
-	}
+	// componentWillMount() {
+	// 	fetch('/articles/')
+	// 		.then(response => response.json())
+	// 		.then(result => {
+	// 			this.setState({ articleList: result })
+	// 			console.log(result)
+	// 		})
+	// 		.catch(e => e);
+	// }
+
+	// render() {
+	// 	return (
+	// 		<div className="App">
+	// 			<header className="App-header">
+	// 				<p>{this.state.articleList ? this.state.articleList[0].body : 'null'}</p>
+	// 			</header>
+	// 		</div>
+	// 	);
+	// }
+
+
 
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<p>{this.state.articleList ? this.state.articleList[0].body : 'null'}</p>
-				</header>
+				<Button type="primary">Button</Button>
 			</div>
 		);
 	}
