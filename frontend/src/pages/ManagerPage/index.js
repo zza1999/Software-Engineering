ed9efcc42463cdd8ba1303a1c3fcd7168e62305b
skrkuromi <987186163@qq.com>
2020-06-06 12:20:36 +0800

    增加路由跳转，对界面分类

---
+++ frontend/src/pages/ManagerPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ManagerPage extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<p>ManagerPage</p>
+			</div>
+		);
+	}
+
+}
+
+export default ManagerPage;
